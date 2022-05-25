bab8de6d5cd6c733d42957cd01738b3ba465c101
Isa Garabana <isabelgarabana@gmail.com>
2022-05-25 18:50:14 +0200

    add elements and styles to search form

--- src/components/FilterByName.js
+++ src/components/FilterByName.js
@@ -1,20 +1,29 @@
+import "../styles/components/filter.scss";
+
 const FilterByName = (props) => {
 	const handleInput = (ev) => {
 		props.handleFilterName(ev.currentTarget.value);
+		ev.preventDefault();
 	};
 	return (
-		<section>
-			<form>
-				<label className="form_label">Filtrar</label>
+		<section className="search">
+			<form className="search--form">
 				<input
-					className="form_input_text"
+					className="search--form__input"
 					type="text"
 					name="name"
 					id="name"
 					onChange={handleInput}
 					value={props.filterName}
+					placeholder="Find a repository..."
 				></input>
 			</form>
+			<div className="button">
+				<button className="button--type">Type</button>
+				<button className="button--language">Language</button>
+				<button className="button--sort">Sort</button>
+				<button className="button--new">New</button>
+			</div>
 		</section>
 	);
 };
